e2c937e0d85e4f311a5414cb2b4a40cf8faec8f7
Abasiono Alexander <ikangenyinabasiono@gmail.com>
2025-11-18 04:01:09 +0100

    Implement wait_type and day_week categorization
    Added wait_type categorization and day mapping.

--- Cat_files.py
+++ Cat_files.py
@@ -12,3 +12,18 @@ airlines['dest_size'] = airlines['dest_size'].str.strip()
 # Verify changes have been effected
 print(airlines['dest_region'].unique())
 print(airlines['dest_size'].unique())
+
+# Create ranges for categories
+label_ranges = [0, 60, 180, np.inf]
+label_names = ['short', 'medium', 'long']
+
+# Create wait_type column
+airlines['wait_type'] = pd.cut(airlines['wait_min'], bins = label_ranges, 
+                                labels =label_names)
+
+# Create mappings dictionary for mapping weekdays to weekday and weekend to weekend and replace
+mappings = {'Monday':'weekday', 'Tuesday':'weekday', 'Wednesday': 'weekday', 
+            'Thursday': 'weekday', 'Friday': 'weekday', 
+            'Saturday': 'weekend', 'Sunday': 'weekend'}
+
+airlines['day_week'] = airlines['day'].map(mappings)
