7d88bff4bffc52998b1a38ebb21d25b26ec69d69
Abasiono Alexander <ikangenyinabasiono@gmail.com>
2025-11-26 18:13:42 +0100

    Add matching functionality for 'italian' cuisine
    Added functionality to compare 'italian' with cuisine types and display matches.

---
+++ Comparing_Strings.py
@@ -0,0 +1,20 @@
+# Import process from thefuzz
+from thefuzz import process
+
+# Store the unique values of cuisine_type in unique_types
+unique_types = restaurants['cuisine_type'].unique()
+
+# Calculate similarity of 'asian' to all values of unique_types
+print(process.extract('asian', unique_types, limit = len(unique_types)))
+
+# Calculate similarity of 'american' to all values of unique_types
+print(process.extract('american', unique_types, limit = len(unique_types)))
+
+# Calculate similarity of 'italian' to all values of unique_types
+print(process.extract('italian', unique_types, limit = len(unique_types)))
+
+# Create a list of matches, comparing 'italian' with the cuisine_type column
+matches = process.extract('italian', restaurants['cuisine_type'], limit = restaurants.shape[0])
+
+# Inspect the first 5 matches
+print(matches[0:5])
